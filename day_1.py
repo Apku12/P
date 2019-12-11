19c30ff5fd825877a8e7e0d56731b02075fabec9
Ajay Patil <39619496+Apku12@users.noreply.github.com>
2019-12-11 17:22:50 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,45 @@
+# First Step Towards Programming
+
+# Following Legacy start with printing Hello World! on Screen.
+print("Hello World!")
+
+'''
+What just hapeend & how it is displayed on screen.
+	Here "Hello World" in quotes specificly is a string.
+		print() is built_in function 
+			Which returns output on Console.
+
+'''
+############################################################################################################
+# Printing Integers and Result Mathamatical Stuff
+print(2) #For Integer
+print(2.0) #For Float
+print(2+2) #For Addition
+print(2*2) #For Multiplication
+print(3/2) #For Division
+
+print(2**2) #For Square
+print(3//2) #For Floor Division(gives number before decimal point e.g: 1.5 => 1)
+print(2%2) #For Remainder
+
+print(2-2+4*3/2) #In Order of Division --> Multipication --> Addition --> Substraction
+
+############################################################################################################
+# Playing with String
+print('hello') # Single Quotes
+print("hello") # Double Quotes 
+
+print('he' + 'll' + "o") # String Concatination(Combinig Strings)
+
+print('2+2') # Integer inside quotes count as sting No opreation will happend
+print('''
+		This is Multi Line String using Triple Quotes.
+	''')
+
+'''
+	Note: Here every time you use new print('') it gives output in new line
+'''
+
+
+print(True)	# Boolean value True
+print(False) # Boolean value False
