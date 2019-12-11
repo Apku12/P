87caa1c20964e7881f22fbf5a6de94561bf407f2
Ajay Patil <39619496+Apku12@users.noreply.github.com>
2019-12-11 17:25:13 +0530

    Add files via upload

---
+++ day_2_practical.py
@@ -0,0 +1,23 @@
+'''
+	Now as you have some basic knowledge about Basic Data Types like string, integers, floats
+	& you know how to print anything on the screen we move forward with storing our data and
+	accessing that data in more productive way.
+'''
+
+# Nomenculture of a variable
+lakshman = 'Lakshman' # Less productive way
+name = 'Lakshman'	# More productive way
+
+my_age = 17 # we can use '_' in our code but can't use space. 
+
+# Wrong variable naming
+2w = 'Wrong variable name' # variable name must not start with any number or special symbol.
+my age = 16 # variable name must not contain space.
+print = 23 # variable name can not be any preseved keywords.
+
+
+# Displaying our variable on the screen.
+name = 'Lakshman'
+age = "19"
+print("My name is " + name) # Here we have used string concatination to combile two values
+print("My age is " + age)
